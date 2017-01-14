8a65d4d562b26d1a9831644aed4e09e16e88265d
mike_neck <jkrt3333@gmail.com>
2017-01-14 15:28:10 +0900

    プロジェクトを発見して自動的に取り込むようにした

---
+++ organize-projects.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "rootProject.name = '${PWD##*/}'" > settings.gradle
+
+find . -type d -maxdepth 4 | \
+  grep -e "\./[a-z]\+\-projects" | \
+  grep -v "/src/\?" | \
+  sed \
+    -e "s/^.\{2\}/include \'\:/g" \
+    -e "s/\//\:/g" \
+    -e "s/\([a-z0-9A-Z]\)$/\1\'/g" >> settings.gradle
