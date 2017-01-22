f2e63ff60af0bb786eb54194f5f14ffa80e48d32
mike_neck <jkrt3333@gmail.com>
2017-01-22 12:14:10 +0900

    jersey-mvc-sample
    add resources

---
+++ java-projects/web-app-projects/jersey-mvc/src/main/java/com/example/HelloResource.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Path("hello")
+public class HelloResource {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_DATE_TIME;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public HelloMessage hello() {
+        final LocalDateTime now = LocalDateTime.now();
+        return new HelloMessage("hello", now.format(FORMATTER));
+    }
+
+    @GET
+    @Path("text")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "hello";
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @XmlRootElement
+    public static class HelloMessage {
+        private String message;
+        private String date;
+    }
+}
