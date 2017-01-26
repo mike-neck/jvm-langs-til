de0ee830a0f0aa5a9d5fcaae450b0af63432c308
mike_neck <jkrt3333@gmail.com>
2017-01-26 22:57:56 +0900

    spring-oauth2
    add top page

---
+++ java-projects/spring-projects/oauth2-app/oauth2-client/src/main/java/com/example/oauth/ClientApp.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@Profile("secret")
+@EnableOAuth2Sso
+public class ClientApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApp.class, args);
+    }
+}
