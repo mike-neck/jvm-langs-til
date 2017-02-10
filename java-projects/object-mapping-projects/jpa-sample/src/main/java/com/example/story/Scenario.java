53c63d2f51abcaa37adbac753c602c71f8f3ad06
mike_neck <jkrt3333@gmail.com>
2017-02-10 22:28:48 +0900

    jpa-project Storyアノテーション作成した

--- java-projects/object-mapping-projects/jpa-sample/src/main/java/com/example/story/Scenario.java
+++ java-projects/object-mapping-projects/jpa-sample/src/main/java/com/example/story/Scenario.java
@@ -15,6 +15,13 @@
  */
 package com.example.story;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
 public @interface Scenario {
 
     Story value();
