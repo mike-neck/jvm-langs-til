28a4dfc00627982c69ee4f653014f5e19cf9a12e
mike_neck <jkrt3333@gmail.com>
2017-03-01 00:15:29 +0900

    crate-db crate-dbのサンプルのDDLとDML(2)

--- java-projects/object-mapping-projects/crate-sample/src/main/resources/sample-crate-ddl.sql
+++ java-projects/object-mapping-projects/crate-sample/src/main/resources/sample-crate-ddl.sql
@@ -6,3 +6,22 @@ CREATE TABLE student (
 )
 WITH (number_of_replicas = 2, column_policy = 'strict'
 );
+
+CREATE TABLE books (
+  id     long PRIMARY KEY,
+  title  string NOT NULL INDEX USING PLAIN,
+  author string NOT NULL INDEX USING PLAIN
+) WITH (number_of_replicas = 2, column_policy = 'strict'
+);
+
+CREATE TABLE rental (
+  id         long PRIMARY KEY,
+  student_id long,
+  book_id    long,
+  period     object ( STRICT ) AS (
+  since TIMESTAMP NOT NULL,
+  to_be_returnes TIMESTAMP NOT NULL,
+  returned TIMESTAMP
+  )
+) WITH (number_of_replicas = 2, column_policy = 'strict'
+);
--- java-projects/object-mapping-projects/crate-sample/src/main/resources/sample-crate-dml.sql
+++ java-projects/object-mapping-projects/crate-sample/src/main/resources/sample-crate-dml.sql
@@ -1,5 +1,30 @@
 INSERT INTO student (id, last_name, first_name, admission_year) VALUES (1, '山田', '太郎', 2016);
 INSERT INTO student (id, last_name, first_name, admission_year) VALUES (2, '山田', '華子', 2015);
+INSERT INTO student (id, last_name, first_name, admission_year) VALUES (3, '高田', '純次', 2017);
 
 SELECT *
 FROM student;
+
+
+INSERT INTO books (id, title, author) VALUES (1, 'ゴリオ爺さん', 'バルザック');
+INSERT INTO books (id, title, author) VALUES (2, '赤と黒', 'スタンダール');
+INSERT INTO books (id, title, author) VALUES (3, '異邦人', 'カミュ');
+INSERT INTO books (id, title, author) VALUES (4, '失われた時を求めて', 'プルースト');
+INSERT INTO books (id, title, author) VALUES (5, '惡の華', 'ボードレール');
+
+INSERT INTO rental (id, student_id, book_id, period)
+VALUES (1, 1, 3, { since = '2017-02-21T04:00:00Z', to_be_returnes = '2017-02-28T04:00:00Z' });
+INSERT INTO rental (id, student_id, book_id, period) VALUES
+  (2, 1, 5, { since = '2017-02-21T04:00:00Z', to_be_returnes = '2017-02-28T04:00:00Z',
+   returned = '2017-02-26T07:00:00Z' });
+INSERT INTO rental (id, student_id, book_id, period)
+VALUES (3, 2, 1, { since = '2017-02-22T04:00:00Z', to_be_returnes = '2017-03-01T01:20:00Z' });
+
+SELECT
+  s.last_name,
+  s.first_name,
+  b.title
+FROM
+  student s
+  JOIN rental r ON s.id = r.student_id
+  JOIN books b ON r.book_id = b.id;
