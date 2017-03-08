50c7b1e275c3aa273ac89a1c7869fc6a8f4293da
mike_neck <jkrt3333@gmail.com>
2017-03-09 03:35:01 +0900

    test-containers 🍥Validated になってなかった

--- java-projects/spring-projects/test-containers/src/main/java/com/example/TodoController.java
+++ java-projects/spring-projects/test-containers/src/main/java/com/example/TodoController.java
@@ -21,6 +21,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotNull;
@@ -46,7 +47,7 @@ public class TodoController {
     }
 
     @RequestMapping(method = { RequestMethod.POST }, consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE }, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<?> create(@RequestBody final CreateTodoParameter parameter, final BindingResult result) {
+    public ResponseEntity<?> create(@RequestBody @Validated final CreateTodoParameter parameter, final BindingResult result) {
         if (result.hasErrors()) {
             final TodoNotFoundException.ErrorMessages messages = result.getFieldErrors()
                     .stream()
