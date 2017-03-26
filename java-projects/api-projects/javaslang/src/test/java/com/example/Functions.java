44c447f27ee8114d1347b4d368aa896bb4a08944
mike_neck <jkrt3333@gmail.com>
2017-03-26 09:32:10 +0900

    javaslangことはじめ - Function Composition

---
+++ java-projects/api-projects/javaslang/src/test/java/com/example/Functions.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2017 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import javaslang.Function1;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Functions {
+
+    @Test
+    void composition() {
+        final Function1<char[], String> toString = String::new;
+        final Function1<String, DateTimeFormatter> toFormatter = DateTimeFormatter::ofPattern;
+        final Function1<DateTimeFormatter, Function1<LocalDate, String>> formatter = f -> f::format;
+
+        final Function1<char[], Function1<LocalDate, String>> fun1 = toString.andThen(toFormatter).andThen(formatter);
+        final Function1<char[], Function1<LocalDate, String>> fun2 = formatter.compose(toFormatter).compose(toString);
+
+        final char[] chars = {'u', 'u', 'u', 'u', '/', 'M', 'M', '/', 'd', 'd'};
+        final Function1<LocalDate, String> f1 = fun1.apply(chars);
+        final Function1<LocalDate, String> f2 = fun2.apply(chars);
+
+        final LocalDate localDate = LocalDate.of(2017, 3, 26);
+        final String res1 = f1.apply(localDate);
+        final String res2 = f2.apply(localDate);
+
+        assertEquals("2017/03/26", res1);
+        assertEquals(res1, res2);
+    }
+}
